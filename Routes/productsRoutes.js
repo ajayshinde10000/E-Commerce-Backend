1ba1c9e13343c89cf20d55d76777dafa33b7f918
Ajay <ajay@angularminds.in>
2023-07-11 14:13:17 +0530

    Update Profile Picture Bug Fixed

--- Routes/productsRoutes.js
+++ Routes/productsRoutes.js
@@ -6,7 +6,6 @@ import multer from 'multer';
 
 const router = express.Router();
 
-
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, 'Product-Images/'); // Specify the destination folder for storing the uploaded images
